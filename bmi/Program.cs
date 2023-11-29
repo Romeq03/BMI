ee35c4fca49f0b2850b671ec4a033c555039ce20
Romeq03 <romekbartek18@gmail.com>
2023-11-29 01:20:52 +0100

    Dodaj pliki projektów.

---
+++ bmi/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+
+    public static void Bmi()
+    {
+        Console.WriteLine("Enter your weight in kilograms: ");
+        double weight = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter your height in meters, e.g. 1.90: ");
+        double hight = Convert.ToDouble(Console.ReadLine());
+        double bmi = weight / (hight * hight);
+        Console.WriteLine(bmi);
+
+    }
+    static void Main(string[] args)
+    {
+        Bmi();
+    }
+}
